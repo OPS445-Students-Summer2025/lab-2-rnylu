6e92991a072866b5dcf0583c6fe41d9e6bb98555
Ronald Lu <rlu22@myseneca.ca>
2025-05-18 00:26:49 -0400

    Added lab2f.py to the repo.

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Ronald Lu
+# Author ID: 134120179
+# Date Created: 2025/05/18
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
