2ce6339f7f4ffaeeea4a90e7ba3592576454d318
Ronald Lu <rlu22@myseneca.ca>
2025-05-18 00:27:16 -0400

    Added lab2g.py to the repo.

---
+++ lab2g.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Author: Ronald Lu
+# Author ID: 134120179
+# Date Created: 2025/05/18
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+    while timer != 0:
+        print(timer)
+        timer = timer -1
+    print("blast off!")
+    sys.exit()
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
