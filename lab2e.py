678457f92e8a3e7f6a49f9613a62b1fb6e16f709
Ronald Lu <rlu22@myseneca.ca>
2025-05-18 00:26:22 -0400

    Added lab2e.py to the repo.

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Author: Ronald Lu
+# Author ID: 134120179
+# Date Created: 2025/05/17
+
+timer = int(10)
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
